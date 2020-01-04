707092f44d01febefe8b4bd5bd1b9301378830f4
Arun Karuppiah <10533185@mydbs.ie>
2020-01-04 13:46:54 +0000

    changes made in route

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -6,10 +6,11 @@ from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
-bp = Blueprint('blog', __name__)
+bp = Blueprint('blog', __name__,url_prefix='/blog')
 
 
-@bp.route('/')
+@bp.route('/index')
+
 def index():
     db = get_db()
     posts = db.execute(
