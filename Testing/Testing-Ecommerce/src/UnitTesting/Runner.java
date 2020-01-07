316ac4a79db3392b49f7a53bf22b617f561e002d
Arun Karuppiah <10533185@mydbs.ie>
2020-01-07 06:03:57 +0000

    Testing

---
+++ Testing/Testing-Ecommerce/src/UnitTesting/Runner.java
@@ -0,0 +1,12 @@
+package UnitTesting;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({Register.class,Login.class,Cart.class,Review.class})
+public class Runner {
+
+}
