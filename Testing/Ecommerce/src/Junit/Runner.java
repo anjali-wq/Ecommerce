ca8cc31d5f4d5c95f04cdcda3bf28ec3872c9e36
Arun Karuppiah <10533185@mydbs.ie>
2020-01-07 06:01:00 +0000

    Change in workspace for Testing code

--- Testing/Ecommerce/src/Junit/Runner.java
+++
@@ -1,9 +0,0 @@
-package Junit;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({Register.class,Login.class,Cart.class,Review.class})
-public class Runner {
-
-}
